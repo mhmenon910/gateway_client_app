a217bcd20209619de27b3f679c7c6cb2f2b0fedd
Manoj Menon <mhmenon910@gmail.com>
2018-03-07 18:12:12 +0530

    rubocop manual

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -1,8 +1,8 @@
 class DefaultController < ApplicationController
 
   def process_request  # mocking amount debited from account sending success status and returning statuses
-  	@encrypted_string_from_client = params["msg"]
-  	@result_from_client = EncryptDecryptString.new(@encrypted_string_from_client).decryption
+    @encrypted_string_from_client = params["msg"]
+    @result_from_client = EncryptDecryptString.new(@encrypted_string_from_client).decryption
     @payload_to_client = EncryptDecryptString.new(@result_from_client).encryption
     response = {}
     response["payload_to_client"] = @payload_to_client
